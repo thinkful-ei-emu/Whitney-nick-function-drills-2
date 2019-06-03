71c863146b086239ac1c7fbb6be126cb6e978ff7
Nicholas Gunter <ygnick@live.com>
2019-06-03 16:10:29 -0400

    added beyond function

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ return `${firstThree}${lastTwo}`;
 }
 
 console.log(jediName('Beyonce', 'Knowles'));
+
+function beyond(num) {
+    if (num === Infinity || num === -Infinity){
+        console.log("And beyond")
+    }
+    else if (num > 0){
+      console.log('To infinity')
+    }
+    else if (num < 0){
+      console.log('To negative infinity')
+    }
+    else if (num === 0){
+      console.log('Staying home')
+    }
+}
+
+
+
